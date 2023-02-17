1a1683e5483185612978115405e142248205c4f2
Juan <juhernandezm@unal.edu.co>
2023-02-16 19:26:43 -0500

    toggle inactive listo

--- index.js
+++ index.js
@@ -7,27 +7,77 @@ class Questions{
         this.number=numberAt
     }
 }
-let q1= new Questions("How many team members can I invite?", "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.", 1)
-let q2= new Questions("What is the maximum file upload size?","No more than 2GB. All files in your account must fit your allotted storage space.", 2)
-let q3= new Questions("How do I reset my password?","Click \“Forgot password\” from the login page or \“Change password\” from your profile page. A reset link will be emailed to you.", 3)
-let q4= new Questions("Can I cancel my subscription?","Yes! Send us a message and we’ll process your request no questions asked.", 4)
-let q5= new Questions("Do you provide additional support?","Chat and email support is available 24/7. Phone lines are open during normal business hours.", 5)
+let q1= new Questions("How many team members can I invite?", "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.", "q1")
+let q2= new Questions("What is the maximum file upload size?","No more than 2GB. All files in your account must fit your allotted storage space.", "q2")
+let q3= new Questions("How do I reset my password?","Click \“Forgot password\” from the login page or \“Change password\” from your profile page. A reset link will be emailed to you.", "q3")
+let q4= new Questions("Can I cancel my subscription?","Yes! Send us a message and we’ll process your request no questions asked.", "q4")
+let q5= new Questions("Do you provide additional support?","Chat and email support is available 24/7. Phone lines are open during normal business hours.", "q5")
 
 qArray.push(q1, q2, q3, q4, q5)
 
+createQuestions()
 function createQuestions(){
     qArray.forEach((q)=>{
-        let structure=`<div class="questionWrap" id="q${q.number}">
-        <div class="titleContainer">
-          <p class="questionTitle">${q.question}</p>
-          <img src="./images/icon-arrow-down.svg" alt="arrow down" class="arrowDown">
-          <div class="question inactive q${q.number}">
-            ${q.answer}
-          </div>
-        </div>`
+        let structure=`
+        <div class="questionWrap" id="wrap${q.number}">
+            <div class="titleContainer" >
+                <p id="title${q.number}" class="questionTitle">${q.question}</p>
+                <img id="img${q.number}" src="./images/icon-arrow-down.svg" alt="arrow down" class="arrowDown">
+            </div>
+                <div class="inactive question ${q.number}" id="ans${q.number}">
+                    ${q.answer}
+                </div>
+        </div>
+        `
         cardQ.innerHTML+=structure
     })
-    wraps=document.querySelectorAll('.questionWrap')
-    console.log(wraps)
+    titles=document.querySelectorAll('.questionTitle')
+    arrows=document.querySelectorAll('.arrowDown')
+    eventListenersTitles(titles)
+    eventListenersArrows(arrows)
 }
-createQuestions()
+function eventListenersTitles(objectArray){
+    objectArray.forEach((object)=>{
+        object.addEventListener('click', (title)=>{
+            let idselectedTitle=title.target.id
+            for(let i=0; i<qArray.length; i++){
+                if(idselectedTitle==`title${qArray[i].number}`){
+                    element=qArray[i]
+                    idElement=qArray[i].number
+                    let answerSelected=document.querySelector(`#ans${idElement}`)
+                    let wrapSelected=document.querySelector(`#wrap${idElement}`)
+                    inactiveLogic()
+                    answerSelected.classList.toggle('inactive')
+                    wrapSelected.classList.toggle('modifier')
+                    object.classList.toggle('titlemodified')
+                    console.log('correcto')
+                }
+            }
+
+        })
+    })
+}
+function eventListenersArrows(objectArray){
+    objectArray.forEach((object)=>{
+        object.addEventListener('click', (arrow)=>{
+            let idSelectedArrow=arrow.target.id
+            for(let i=0; i<qArray.length; i++){
+                if(idSelectedArrow==`img${qArray[i].number}`){
+                    element=qArray[i]
+                    idElement=qArray[i].number
+                    let answerSelected=document.querySelector(`#ans${idElement}`)
+                    let wrapSelected=document.querySelector(`#wrap${idElement}`)
+                    let titleSelected=document.querySelector(`#title${idElement}`)
+                    inactiveLogic()
+                    answerSelected.classList.toggle('inactive')
+                    wrapSelected.classList.toggle('modifier')
+                    titleSelected.classList.toggle('titlemodified')
+                    console.log('correcto')
+                }
+            }
+        })
+    })
+}
+function inactiveLogic(){
+
+}
